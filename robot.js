92cda169a9f026f23fd00e9623be9d330e9ac586
Bill Nagbe <BillyNagbe@gmail.com>
2025-06-29 07:50:01 -0500

    fin solution for a concise route pattern

--- robot.js
+++ robot.js
@@ -113,9 +113,36 @@ const mailRoute = [
 
 
 function routeRobot(state, memory) { // goal to make the robot route
-  if(memory.length == 0) { memory = mailRoute};
-  return {direction: memory[0], memory: memory.slice(1)};
+  if(memory.length == 0) { memory = mailRoute}; // if the passed variable length member is 0 return the mailRoute 
+  return {direction: memory[0], memory: memory.slice(1)}; // 
 }
 
 
-runRobot(VillageState.random(), routeRobot, []);
+function findRoute(graph, from, to) { // finds a route  from a graph[world] using the current[from] location to a destination[to]
+  let work = [{at: from, route: []}]; // data structure to pass values to
+  for(let i = 0; i < work.length; i++) { // for loop based on the passed values length member
+    let {at, route} = work[i]; // destructures the data structure 
+    for(let place of graph[at]) { // loops thru the at property in the passed graph value
+      if(place == to) return route.concat(place); // if the current value equals the inital passed value add the current value to the route
+      if(!work.some(w => w.at == place)) { // if not true 
+        work.push({at: place, route: route.concat(place)}); // push another set of data to the data structure we create at the start
+      }
+    }
+  }
+}
+
+function goalOrientedRobot({place, parcels}, route) { // to check if a route has been found and update internal state 
+    if(route.length === 0) { // if the passed variable length member is 0
+      let parcel = parcels[0]; // first item of the passed object variable
+      if(parcel.place != place) { // if not true
+        route = findRoute(roadGraph, place, parcel.place); // set the passed variable using a helper function to update internal state
+      }
+      else {
+        route = findRoute(roadGraph, place, parcel.address); // set the passed variable using a helper function to update internal state
+      }
+    }
+    return {direction: route[0], memory: route.slice(1)}; // returns an object with an updated internal state
+}
+
+
+runRobot(VillageState.random(), goalOrientedRobot, []);
